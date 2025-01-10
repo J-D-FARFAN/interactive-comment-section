6cb682357be4027015a0d9f48e5b977116aaf7c0
Juan David Farfan <juan.frontendMJ@gmail.com>
2025-01-10 17:04:23 -0500

    Agregar íconos de edición y eliminación en los comentarios, y mejorar el estilo y la funcionalidad de la sección de comentarios.

--- src/assets/components/comments.jsx
+++ src/assets/components/comments.jsx
@@ -2,6 +2,8 @@ import "./stylesComponents/comments.css";
 import IconPlus from "./Icons/plus";
 import IconMinus from "./Icons/minus";
 import IconReply from "./Icons/reply";
+import IconEdit from "./Icons/edit";
+import IconDelete from "./Icons/delete";
 import imageDefault from "/public/images/avatars/image-amyrobson.webp";
 import { useState } from "react";
 import { UserComments } from "./userComments";
@@ -46,24 +48,39 @@ export function Comments() {
                 <div className="bx-avatar">
                   <img src={imageDefault} alt="avatar" className="avatar" />
                 </div>
-
                 <span className="nameUser">amyrobson</span>
-
-                <span className="createdComments">1 month ago</span>
+                <span className="meUser">you</span>
+                <span className="createdComments">Just now</span>
+                {/* Cambia dinámicamente */}
               </section>
 
-              <div
-                className="bx-replyComments"
-                onClick={handleClickShowAddedComment}
-              >
-                <span className="bx-reply">
-                  <IconReply />
-                </span>
+              <section className="bx-btns-controler">
+                <div className="bx-delete">
+                  <span className="iconDelete">
+                    <IconDelete />
+                  </span>
+                  <span className="delete">Delete</span>
+                </div>
+                <div className="bx-edit">
+                  <span className="iconEdit">
+                    <IconEdit />
+                  </span>
+                  <span className="edit">Edit</span>
+                </div>
 
-                <span className="reply">
-                  {showAddedComment ? "Close" : "Reply"}
-                </span>
-              </div>
+                <div
+                  className="bx-replyComments"
+                  onClick={handleClickShowAddedComment}
+                >
+                  <span className="bx-reply">
+                    <IconReply />
+                  </span>
+
+                  <span className="reply">
+                    {showAddedComment ? "Close" : "Reply"}
+                  </span>
+                </div>
+              </section>
             </header>
 
             <div className="contentComment">
@@ -76,14 +93,17 @@ export function Comments() {
           </section>
         </section>
       </article>
+      <section className="content__comments-user">
+        <span className="line"></span>
 
-      <div
-        className={`user-comments-container ${
-          showAddedComment ? "showAdded" : "closeAdded"
-        }`}
-      >
-        {showAddedComment && <UserComments TextBtn={"REPLY"} />}
-      </div>
+        <div
+          className={`user-comments-container ${
+            showAddedComment ? "showAdded" : "closeAdded"
+          }`}
+        >
+          {showAddedComment && <UserComments TextBtn={"REPLY"} />}
+        </div>
+      </section>
     </>
   );
 }
